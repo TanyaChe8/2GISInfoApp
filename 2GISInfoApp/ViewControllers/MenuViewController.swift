a41874125d6ade19bfef2874ec76a19adb4c2ac3
KaKaIn4ik <82812476+KaKaIn4ik@users.noreply.github.com>
2022-03-31 01:08:09 +0700

    Update hybrid commit

--- 2GISInfoApp/ViewControllers/MenuViewController.swift
+++ 2GISInfoApp/ViewControllers/MenuViewController.swift
@@ -12,18 +12,24 @@ class MenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
+    @IBAction func newInfoPressedButton() {
+        performSegue(withIdentifier: "TransitionHybrid", sender: nil)
+    }
+
+    @IBAction func quizPressedButton() {
+        performSegue(withIdentifier: "TransitionQuiz", sender: nil)
+    }
+    
+    @IBAction func faqPressedButton() {
+        performSegue(withIdentifier: "TransitionFAQ", sender: nil)
+    }
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
