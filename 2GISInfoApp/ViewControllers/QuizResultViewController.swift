2de926ff10d90a28316d0b97cea02b6c2faa331b
Черненко Татьяна Анатольевна <t.chernenko@2Gis.ru>
2022-04-02 21:21:47 +0700

    Fix crash on the last quiz screen

--- 2GISInfoApp/ViewControllers/QuizResultViewController.swift
+++ 2GISInfoApp/ViewControllers/QuizResultViewController.swift
@@ -8,29 +8,29 @@
 import UIKit
 
 class QuizResultViewController: UIViewController {
-
+	
 	@IBOutlet weak var cityLabel: UILabel!
 	@IBOutlet weak var cityInfoLabel: UILabel!
-	
-		var answersChosen: [Answer]!
-		private var resultCity: City?
-		
-		override func viewDidLoad() {
-			super.viewDidLoad()
-			
-			resultCity = getResults()
-			cityLabel.text = "ВЫ - \(resultCity?.rawValue ?? "?")!"
-			cityInfoLabel.text = resultCity?.definition ?? "?"
-			
-		}
 
-		private func getResults() -> City? {
-			var result: [City: Int] = [:]
-			
-			let cities = answersChosen.map { $0.city }
-			for index in cities {
-				result[index] = (result[index] ?? 0) + 1
-			}
-			return result.sorted { $0.value > $1.value }.first?.key
-		}
+	var answersChosen: [Answer]!
+	private var resultCity: City?
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		navigationItem.hidesBackButton = true
+
+		resultCity = getResults()
+		cityLabel.text = "ВЫ - \(resultCity?.rawValue ?? "?")!"
+		cityInfoLabel.text = resultCity?.definition ?? "?"
 	}
+
+	private func getResults() -> City? {
+		var result: [City: Int] = [:]
+		
+		let cities = answersChosen.map { $0.city }
+		for index in cities {
+			result[index] = (result[index] ?? 0) + 1
+		}
+		return result.sorted { $0.value > $1.value }.first?.key
+	}
+}
